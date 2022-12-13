36f56cac2b39bee6945d95e30e420048b79c9322
Ethan <et.l.nicholas@gmail.com>
2022-12-13 13:19:45 +1100

    remove post data from App.js

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import { db } from './firebase';
 import { collection, onSnapshot } from 'firebase/firestore';
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username:'Dan Hunter', 
-    //   caption:' The splashing moment', 
-    //   imageUrl:'https://images.unsplash.com/photo-1594372365401-3b5ff14eaaed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-    //   alt:'splash'
-    // },
-    // {
-    //   username:'Micheal Black', 
-    //   caption:' The colorful drawing',
-    //   imageUrl:'https://images.unsplash.com/photo-1598112152680-0c39a2928c64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    //   alt:'colorful'
-    // },
-    // {
-    //   username:'Warren Smith',
-    //   caption:' Art work -- a paint',
-    //   imageUrl:'https://images.unsplash.com/photo-1529405456913-530d6c6d2966?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    //   alt:'paint'
-    // }
-  ]);
+  const [posts, setPosts] = useState([]);
   // useEffect -> Runs a piece of code based on a specific conditions
   useEffect(()=>{
 
