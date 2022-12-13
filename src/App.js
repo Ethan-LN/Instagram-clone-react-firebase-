c5b8a9aae492423646f1f5d95deddef34ed5a237
Ethan <et.l.nicholas@gmail.com>
2022-12-13 11:24:09 +1100

    link the post with the firebase data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post';
+import { db } from './firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 function App() {
   const [posts, setPosts] = useState([
-    {
-      username:'Dan Hunter', 
-      caption:' The splashing moment', 
-      imageUrl:'https://images.unsplash.com/photo-1594372365401-3b5ff14eaaed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-      alt:'splash'
-    },
-    {
-      username:'Micheal Black', 
-      caption:' The colorful drawing',
-      imageUrl:'https://images.unsplash.com/photo-1598112152680-0c39a2928c64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      alt:'colorful'
-    },
-    {
-      username:'Warren Smith',
-      caption:' Art work -- a paint',
-      imageUrl:'https://images.unsplash.com/photo-1529405456913-530d6c6d2966?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-      alt:'paint'
-    }
+    // {
+    //   username:'Dan Hunter', 
+    //   caption:' The splashing moment', 
+    //   imageUrl:'https://images.unsplash.com/photo-1594372365401-3b5ff14eaaed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+    //   alt:'splash'
+    // },
+    // {
+    //   username:'Micheal Black', 
+    //   caption:' The colorful drawing',
+    //   imageUrl:'https://images.unsplash.com/photo-1598112152680-0c39a2928c64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    //   alt:'colorful'
+    // },
+    // {
+    //   username:'Warren Smith',
+    //   caption:' Art work -- a paint',
+    //   imageUrl:'https://images.unsplash.com/photo-1529405456913-530d6c6d2966?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    //   alt:'paint'
+    // }
   ]);
+  // useEffect -> Runs a piece of code based on a specific conditions
+  useEffect(()=>{
+
+    const unsub = onSnapshot(collection(db,'posts'), (snapshot) => {
+       setPosts(snapshot.docs.map(doc => doc.data()));
+       })
+  },[]);
+
 
   return (
     <div className='app'>
@@ -38,7 +48,7 @@ function App() {
         posts.map(post =>(
           <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} alt={post.alt}/>
         ))
-      }
+      }/
       </div>
 
   );
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from 'firebase/firestore';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDO2esNrb9fXKAfDKQ1m06cM4tVX2VWmes",
+    authDomain: "instagram-clone-42aea.firebaseapp.com",
+    projectId: "instagram-clone-42aea",
+    storageBucket: "instagram-clone-42aea.appspot.com",
+    messagingSenderId: "983844260608",
+    appId: "1:983844260608:web:64663d811f42ea22e181e5",
+    measurementId: "G-S0CFN9XXR4"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
