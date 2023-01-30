abd3372abacd8ca28609be4cfe7183aa8ea2cc6c
Ethan <et.l.nicholas@gmail.com>
2023-01-30 14:30:58 +1100

    username validation from textfield

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,11 +5,36 @@ import ValidationTextFields from './ValidateFrom';
 import './Form.css';
 import { Button, input } from '@mui/material';
 import { bgcolor } from '@mui/system';
+import { db,auth } from '../firebase';
+import { useState } from 'react';
 
 export default function Form(props) {
-  const SignUp = () =>{
-
+  const signUp = (event) =>{
+    event.preventDefault();
+    // auth.createUserWithEmailAndPassword(props.email,props.password)
+    // .catch((error) => alert(error.message));
   }
+
+  const [username, setUserName] = useState("");
+  const [nameValid, setNameValid] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [pwdValid, setPwdValid] = useState(false);
+
+  const handleValidationUsername = (e) => {
+    //set value to user input
+    setUserName(e.target.username);
+    
+    //define regex     
+    const reg = new RegExp("[a-z]");
+    
+    //test whether input is valid
+    setNameValid(reg.test(e.target.value));
+};
+
   return (
     <form className='form__layout'>
       <img
@@ -19,19 +44,23 @@ export default function Form(props) {
     />
       <div className='textfield__layout'>
         <TextField
-          required
+          required={true}
+          error={!nameValid}
           id="filled-required"
           label="username"
+          value={username}
           variant="filled"
+          onChange={(e) => handleValidationUsername(e)}
         />
         <TextField
-          required
+          required = {true}
           id="filled-email"
           label="email"
+          // value={value}
           variant="filled"
+          // onChange={(e) => handleValidation(e)}
         />
         <TextField
-          fullWidth={true}
           required
           id="filled-password-input"
           label="password"
@@ -39,8 +68,17 @@ export default function Form(props) {
           autoComplete="current-password"
           variant="filled"
         />
+
+        <TextField
+          required
+          id="filled-password-input"
+          label="confrim password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
       </div>
-        <Button fullWidth={true} className="btn__signUp" onClick={SignUp}>Sign Up</Button>
+        <Button type="submit" fullWidth={true} className="btn__signUp" onClick={signUp}>Sign Up</Button>
     </form>
   );
 }
